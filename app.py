36abfbb1e3b196f6d0278cbf8ce6f6e76a85a231
Banavathu Siva Sankara Vara Prasad <bsivasankarvaraprasad@gmail.com>
2025-03-09 23:16:54 +0530

    app.py last line changed

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def toggle_detection():
 
 if __name__ == '__main__':
     #app.run(debug=True, threaded=True)
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
